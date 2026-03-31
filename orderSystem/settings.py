7182ff851f0b6f9c447f82806f877119a791ca55
Yibor <yibor0312@gmail.com>
2026-03-31 10:59:48 +0800

    deploy: switch to sqlite and upload database

--- orderSystem/settings.py
+++ orderSystem/settings.py
@@ -58,16 +58,18 @@ WSGI_APPLICATION = 'orderSystem.wsgi.application'
 # 4. 資料庫設定 (MySQL)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'project', 
-        'USER': 'root', 
-        'PASSWORD': '1234', 
-        'HOST': 'localhost', 
-        'PORT': '8888', 
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql', 
+        # 'NAME': 'project', 
+        # 'USER': 'root', 
+        # 'PASSWORD': '1234', 
+        # 'HOST': 'localhost', 
+        # 'PORT': '8888', 
+        # 'OPTIONS': {
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        #     'charset': 'utf8mb4',
+        # }
     }
 }
 
